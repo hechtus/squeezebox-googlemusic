109c0a2b38f4f8bc157ec682a9cb5bf5a7cab8df
hechtus <rhe@sysgo.com>
2013-08-01 09:42:51 +0200

    Metadata expects duration in seconds. Minor clean-up.

--- GoogleMusic/ProtocolHandler.pm
+++ GoogleMusic/ProtocolHandler.pm
@@ -3,9 +3,6 @@ package Plugins::GoogleMusic::ProtocolHandler;
 use strict;
 use base qw(Slim::Player::Protocols::HTTP);
 
-use Scalar::Util qw(blessed);
-use URI::Escape qw(uri_escape_utf8);
-
 use Slim::Player::Playlist;
 use Slim::Utils::Log;
 use Slim::Utils::Misc;
@@ -25,13 +22,6 @@ sub getFormatForURL { 'mp3' }
 sub scanUrl {
 	my ($class, $url, $args) = @_;
 
-	my $song = $args->{song};
-
-	# To support seeking set duration and bitrate
-	$song->duration($song->currentTrack()->secs);
-	# Always 320k at Google Music
-	$song->bitrate(320000);
-
 	$args->{cb}->( $args->{song}->currentTrack() );
 }
 
@@ -42,9 +32,14 @@ sub getNextTrack {
 	  
 	my ($id) = $url =~ m{^googlemusic:track:(.*)$};
 
+	# TBD: Error handling. Call errorCb or errors.
 	my $trackURL = $googleapi->get_stream_url($id);
 
 	$song->streamUrl($trackURL);
+	# To support seeking set duration and bitrate
+	$song->duration($song->currentTrack()->secs);
+	# Always 320k at Google Music
+	$song->bitrate(320000);
 
 	$successCb->();
 }
@@ -54,25 +49,24 @@ sub canDirectStreamSong {
 	
 	# We need to check with the base class (HTTP) to see if we
 	# are synced or if the user has set mp3StreamingMethod
-	return $class->SUPER::canDirectStream( $client, $song->streamUrl(), $class->getFormatForURL() );
+	return $class->SUPER::canDirectStream( $client, $song->streamUrl(), $class->getFormatForURL($song->track->url()) );
 }
 
 sub getMetadataFor {
-	# don't use $_[3] as it is forceCurrent used by AudioScrobbler
-	my ($class, $client, $url, undef, $fetch) = @_; 
+	my ($class, $client, $url) = @_; 
 
 	my $track = $googleapi->get_track($url);
 	my $secs = $track->{'durationMillis'} / 1000;
+	my $image = Plugins::GoogleMusic::Image->uri($track->{'albumArtUrl'});
 
 	return {
 		title    => $track->{'name'},
 		artist   => $track->{'artist'},
 		album    => $track->{'album'},
-		secs     => $secs,
-		duration => sprintf('%d:%02d', int($secs / 60), $secs % 60),
-		cover    => Plugins::GoogleMusic::Image->uri($track->{'albumArtUrl'}),
-		icon     => Plugins::GoogleMusic::Image->uri($track->{'albumArtUrl'}),
-		bitrate  => '320k',
+		duration => $secs,
+		cover    => $image,
+		icon     => $image,
+		bitrate  => '320k CBR',
 		type     => 'MP3 (Google Music)',
 		albumuri => $track->{'myAlbum'}->{'uri'},
 		artistA  => $track->{'myAlbum'}->{'artist'},
