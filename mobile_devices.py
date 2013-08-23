c12851137dd415793a012eedbb7d2c7ea9d482da
hechtus <ronald.hecht@gmx.de>
2013-08-23 13:09:04 +0200

    Provide a small script to list all devices registered on Google Play Music.

---
+++ mobile_devices.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+from gmusicapi import Webclient
+
+def main():
+    parser = argparse.ArgumentParser(description = 'List all devices registered for Google Play Music')
+
+    parser.add_argument('username', help = 'Your Google Play Music username')
+    parser.add_argument('password', help = 'Your very secret password')
+
+    args = parser.parse_args()
+    
+    api = Webclient()
+
+    if not api.login(args.username, args.password):
+        print "Could not login to Google Play Music. Incorrect username or password."
+        return
+
+    for device in api.get_registered_devices():
+        print '%s | %s | %s | %s' % (device['name'],
+                                     device['manufacturer'],
+                                     device['type'],
+                                     device['id'])
+
+    api.logout()
+
+if __name__ == '__main__':
+    main()
